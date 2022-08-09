07df77fee33a15025ff140b92b7999035a83590c
Fernando <hssfernando@hotmail.com>
2022-08-09 09:08:38 -0300

    Commit models

---
+++ models.py
@@ -0,0 +1,36 @@
+#Importação das bibliotecas ORM
+from sqlalchemy import create_engine, Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import datetime
+
+#Criar conexão com banco de dados
+USUARIO = 'root'
+SENHA = ''
+HOST = 'localhost'
+PORT = '3306'
+BANCO = 'apilogin'
+
+CONN = f'mysql+pymysql://{USUARIO}:{SENHA}@{HOST}:{PORT}/{BANCO}'
+
+engine = create_engine(CONN, echo=True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+#Criação das tabelas do banco de dado
+class Pessoa(Base):
+    __tablename__ = 'Pessoa'
+    id = Column(Integer,primary_key=True)
+    nome = Column(String(50))
+    usuario = Column(String(20))
+    senha = Column(String(10))
+
+class Tokens(Base):
+    __tablename__ = 'Tokens'
+    id = Column(Integer, primary_key=True)
+    id_pessoa = Column(Integer, ForeignKey('Pessoa.id'))
+    token = Column(String(100))
+    data = Column(DateTime, default=datetime.datetime.utcnow())
+
+Base.metadata.create_all(engine)
